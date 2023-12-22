4d5d4a61feb91b6b5abe04b7f666912eeec1cff9
YeFan-0 <2389707173@qq.com>
2023-12-22 21:27:25 +0800

    bundle platform addition
    bundle lock --add-platform x86_64-linux

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1703249888';
+const cacheName = 'chirpy-1703250522';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
